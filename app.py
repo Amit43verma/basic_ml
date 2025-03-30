fdf650a07e2a0ac7d93d334b4d2a0c92b66bd343
Amit Verma <144679426+Amit43verma@users.noreply.github.com>
2025-03-30 19:54:21 +0530

    fix cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained model
 with open("model.pkl", "rb") as f:
